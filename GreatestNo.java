1bf582e0a8cf0037b307a335ff4730a600bb55d9
Vignesh24 <116154171+Vignesh578@users.noreply.github.com>
2023-02-06 21:43:56 +0530

    Add files via upload

---
+++ GreatestNo.java
@@ -0,0 +1,29 @@
+package vipro;
+import java.io.*;
+import java.util.Scanner;
+
+public class GreatestNo
+{  
+public static void main(String[] args)   
+{  
+int num1,num2;
+System.out.println("Enter the Number1:");
+Scanner sc = new Scanner(System.in);
+num1=sc.nextInt();
+System.out.println("Enter the Number2:");
+Scanner s = new Scanner(System.in);
+num2=s.nextInt();
+if(num1<num2)  
+{  
+System.out.println(num2+" is the Greatest Number.");  
+}   
+else if (num1>num2)
+{  
+System.out.println(num2+" is the Smallest Number");  
+}  
+else
+{
+	System.out.println("Both Numbers are equal!");  
+}
+}
+}  
---
+++ skip2rev.java
@@ -0,0 +1,13 @@
+package vipro;
+
+public class skip2rev {
+     public static void main(String[] ar)
+     {
+    	 int i=103;
+    	 while(i>2)
+    	 {
+    		 i-=3;    // reverse
+    		 System.out.print(i+" ");
+    	 }
+     }
+}
