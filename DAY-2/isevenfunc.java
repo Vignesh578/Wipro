5a8fcf062f5d56a9d68e35a1c0c65d7fc45eb222
Vignesh24 <116154171+Vignesh578@users.noreply.github.com>
2023-02-07 18:28:54 +0530

    Update isevenfunc.java

--- DAY-2/isevenfunc.java
+++ DAY-2/isevenfunc.java
@@ -1,18 +1,21 @@
 package vipro;
+import java.util.*;
 public class isevenfunc   
 {  
 public static void main(String[] args)   
 {  
-int a = 20;   
-
-int c = add(a);     
+int a,c;   
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the No:");
+a=sc.nextInt();
+c=add(a); 
 System.out.println(c);  
 }  
 
 public static int add(int n1)     
 {  
 int i=0;
-if (n1%2==0)
+if(n1%2==0)
 {
 	i+=2;
 }
